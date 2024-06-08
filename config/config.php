9eb03bb6805a6806b1cd170af0da95be54ecdce7
Evertjan Garretsen <evertjan@mlbrgn.nl>
2024-06-08 19:40:20 +0200

    changes

--- config/config.php
+++ config/config.php
@@ -20,63 +20,78 @@ return [
         ],
 
         'form-checkbox' => [
-            'view'  => 'form-components::{framework}.form-checkbox',
-            'class' => Components\FormCheckbox::class,
+            'view'  => 'form-components::{framework}.checkbox',
+            'class' => Components\Checkbox::class,
         ],
 
         'form-errors' => [
-            'view'  => 'form-components::{framework}.form-errors',
-            'class' => Components\FormErrors::class,
+            'view'  => 'form-components::{framework}.errors',
+            'class' => Components\Errors::class,
         ],
 
         'form-group' => [
-            'view'  => 'form-components::{framework}.form-group',
-            'class' => Components\FormGroup::class,
+            'view'  => 'form-components::{framework}.group',
+            'class' => Components\Group::class,
         ],
 
         'form-input' => [
-            'view'  => 'form-components::{framework}.form-input',
-            'class' => Components\FormInput::class,
+            'view'  => 'form-components::{framework}.input',
+            'class' => Components\Input::class,
+        ],
+
+        'html-editor' => [
+            'view'  => 'form-components::{framework}.html-editor',
+            'class' => Components\HtmlEditor::class,
+        ],
+
+        'inline' => [
+            'view'  => 'form-components::{framework}.inline',
+            'class' => Components\Inline::class,
+        ],
+
+        'captcha' => [
+            'view'  => 'form-components::{framework}.captcha',
+            'class' => Components\Captcha::class,
         ],
 
         'form-input-group' => [
-            'view'  => 'form-components::{framework}.form-input-group',
-            'class' => Components\FormInputGroup::class,
+            'view'  => 'form-components::{framework}.input-group',
+            'class' => Components\InputGroup::class,
         ],
 
         'form-input-group-text' => [
-            'view'  => 'form-components::{framework}.form-input-group-text',
-            'class' => Components\FormInputGroupText::class,
+            'view'  => 'form-components::{framework}.input-group-text',
+            'class' => Components\InputGroupText::class,
         ],
 
         'form-label' => [
-            'view'  => 'form-components::{framework}.form-label',
-            'class' => Components\FormLabel::class,
+            'view'  => 'form-components::{framework}.label',
+            'class' => Components\Label::class,
         ],
 
         'form-radio' => [
-            'view'  => 'form-components::{framework}.form-radio',
-            'class' => Components\FormRadio::class,
+            'view'  => 'form-components::{framework}.radio',
+            'class' => Components\Radio::class,
         ],
 
         'form-range' => [
-            'view'  => 'form-components::{framework}.form-range',
-            'class' => Components\FormRange::class,
+            'view'  => 'form-components::{framework}.range',
+            'class' => Components\Range::class,
         ],
 
         'form-select' => [
-            'view'  => 'form-components::{framework}.form-select',
-            'class' => Components\FormSelect::class,
+            'view'  => 'form-components::{framework}.select',
+            'class' => Components\Select::class,
         ],
 
         'form-submit' => [
-            'view'  => 'form-components::{framework}.form-submit',
-            'class' => Components\FormSubmit::class,
+            'view'  => 'form-components::{framework}.submit',
+            'class' => Components\Submit::class,
         ],
 
         'form-textarea' => [
-            'view'  => 'form-components::{framework}.form-textarea',
-            'class' => Components\FormTextarea::class,
+            'view'  => 'form-components::{framework}.textarea',
+            'class' => Components\Textarea::class,
         ],
     ],
 ];
---
+++ src/Components/Captcha.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Mlbrgn\LaravelFormComponents\Components;
+
+class Captcha extends FormBaseComponent
+{
+}
