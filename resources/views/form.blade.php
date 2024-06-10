4cfdc6cca845db1f39ade11d651b4308b5b1ce32
Evertjan Garretsen <evertjan@mlbrgn.nl>
2024-06-10 21:17:30 +0200

    removed css "inline-space" which puts margin right of direct children that are not a template element.

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,11 +2,6 @@
     method="{{ $spoofMethod ? 'POST' : $method }}"
     {!! $attributes->merge(['class' => $hasError() ? 'needs-validation' : '']) !!}
 >
-    <style>
-        .inline-space > :not(template) {
-            margin-right: 1.25rem;
-        }
-    </style>
 
 @unless(in_array($method, ['HEAD', 'GET', 'OPTIONS']))
     @csrf
