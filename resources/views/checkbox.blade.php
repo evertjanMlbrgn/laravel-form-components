7244a30852b90a9d2d92117418a7746c7694c797
Evertjan Garretsen <evertjan@mlbrgn.nl>
2024-06-12 22:31:11 +0200

    fixed bug in checkbox, conditionally add classes to form control wrapper

--- resources/views/checkbox.blade.php
+++ resources/views/checkbox.blade.php
@@ -1,4 +1,8 @@
-<x-form-control-wrapper :id="$getId()" class="form-check @if(null !== $attributes->get('switch')) form-switch @endif @if(null !== $attributes->get('inline')) form-check-inline @endif">
+<x-form-control-wrapper :id="$getId()" @class([
+    'form-check',
+    'form-switch' => $attributes->get('inline'),
+    'form-check-inline' => $attributes->get('inline')
+])>
 
     <input
         {!! $attributes->merge(['class' => 'form-check-input' . ($hasError($name) ? ' is-invalid' : '')]) !!}
