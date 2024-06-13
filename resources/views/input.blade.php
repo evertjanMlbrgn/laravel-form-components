fece3448057eb461cef170dd4721faa076b74f2d
Evertjan Garretsen <evertjan@mlbrgn.nl>
2024-06-13 11:46:37 +0200

    changes

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,23 +1,4 @@
-@props([
-    'hidden' => false,
-    'required' => false,
-    'floating' => false,
-    'horizontal' => false,
-])
-
-{{--<samp>--}}
-{{--    <p> inside input</p>--}}
-{{--    <p>--}}
-{{--        name {{ $name }}--}}
-{{--        hidden {{ $hidden ? 'yes' : 'no' }},--}}
-{{--        required {{ $required ? 'yes' : 'no'}},--}}
-{{--        floating {{ $floating ? 'yes' : 'no' }}--}}
-{{--        horizontal {{ $horizontal ? 'yes' : 'no' }}--}}
-{{--        errors {{ $errors ?? $errors }}--}}
-{{--    </p>--}}
-{{--</samp>--}}
-
-<x-form-control-wrapper :id="$getId()" >
+<x-form-control-wrapper :parentClasses="$attributes->get('class')" :id="$getId()" >
         <input
             {{ $attributes->class([
             'form-control',
@@ -28,12 +9,11 @@
             value="{{ $value ?? ($type === 'color' ? '#000000' : '') }}"
             name="{{ $name }}"
             id="{{ $getId() }}"
+
             {{-- floating labels won't work without placeholder, yet they never display placeholder either --}}
             @if($floating && !$attributes->has('placeholder'))
                     placeholder="&nbsp;"
             @endif
-            {{-- don't understand why placeholder is automatically added to this input when not floating label --}}
         >
-
 </x-form-control-wrapper>
 
--- resources/views/label.blade.php
+++ resources/views/label.blade.php
@@ -1,10 +1,20 @@
 @aware([
-    'required'
+    'required',
+    'horizontal',
 ])
+
+@php
+    $containsFormControlLg = $horizontal && str_contains($attributes->get('parentClasses'), 'form-control-lg');
+    $containsFormControlSm = $horizontal && str_contains($attributes->get('parentClasses'), 'form-control-sm');
+@endphp
+
 @if ($slot->isNotEmpty())
     <label {{ $attributes->class([
-        'form-label',
-        'required' => $required
+        'form-label' => !$horizontal,
+        'col-form-label' => $horizontal && !$containsFormControlLg && !$containsFormControlSm,
+        'required' => $required,
+        'col-form-label-sm' => $horizontal && $containsFormControlSm,
+        'col-form-label-lg' => $horizontal && $containsFormControlLg
     ]) }}>
         {{ $slot }}
     </label>
