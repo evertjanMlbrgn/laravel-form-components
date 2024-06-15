ecbb179662c29d800f364c72b2f8de66090bf048
Evertjan Garretsen <evertjan@mlbrgn.nl>
2024-06-15 22:29:41 +0200

    added more html editor tests to see if correct classes are set.

--- tests/Feature/HtmlEditorTest.php
+++ tests/Feature/HtmlEditorTest.php
@@ -2,13 +2,15 @@
 
 uses(\Mlbrgn\LaravelFormComponents\Tests\TestCase::class);
 
-it('check the right element as default', function () {
+it('renders html-editor', function () {
     $this->registerTestRoute('html-editor');
 
     $this->visit('/html-editor')
         ->assertResponseOk()
-        ->seeElement('textarea[name="html-editor-1"][required].form-control.html-editor')
-        ->seeElement('textarea[name="html-editor-2"].form-control.html-editor')
+        ->seeElement('textarea[name="html-editor-1"][required][id="auto_id_html-editor-1"].form-control.html-editor')
+        ->seeElement('textarea[name="html-editor-2"][id="html-editor-2"].form-control.html-editor')
+        ->seeElement('label[for="auto_id_html-editor-1"].form-label.required')
+        ->seeElement('label[for="html-editor-2"].form-label')
         ->seeText('html-content-1')
         ->seeText('html-content-2');
 });
--- tests/Feature/views/html-editor.blade.php
+++ tests/Feature/views/html-editor.blade.php
@@ -7,10 +7,8 @@ $target = ['checkbox' => ['a']];
         <x-form-html-editor name="html-editor-1" label="label 1" required>
             html-content-1
         </x-form-html-editor>
-        <x-form-html-editor name="html-editor-2" value="html 2" label="label 2">
+        <x-form-html-editor name="html-editor-2" id="html-editor-2" label="label 2">
             html-content-2
         </x-form-html-editor>
     @endbind
-
-    <x-form-submit />
 </x-form-form>
