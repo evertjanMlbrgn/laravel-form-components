2b11c1ea9522ca16095a5564a0dae9da0456e290
Evertjan Garretsen <evertjan@mlbrgn.nl>
2024-06-19 01:11:09 +0200

    Added same class constructor and properties and traits in HtmlEditor as in Textarea

--- src/View/Components/HtmlEditor.php
+++ src/View/Components/HtmlEditor.php
@@ -2,6 +2,42 @@
 
 namespace Mlbrgn\LaravelFormComponents\View\Components;
 
-class HtmlEditor extends Input
+use Mlbrgn\LaravelFormComponents\Traits\HandlesDefaultAndOldValue;
+use Mlbrgn\LaravelFormComponents\Traits\HandlesValidationErrors;
+use Mlbrgn\LaravelFormComponents\View\FormBaseComponent;
+
+class HtmlEditor extends FormBaseComponent
 {
+    use HandlesDefaultAndOldValue;
+    use HandlesValidationErrors;
+
+    public $value;
+
+    public function __construct(
+        public string $name = '',
+        $bind = null,
+        $default = null,
+        $language = null,
+        public string $label = '',
+        public string $type = 'text',
+        bool $showErrors = true,
+        public bool $horizontal = false,
+        public bool $floating = false,
+//        public bool $required = false,
+        public bool $hidden = false,
+        public string $classLabel = '',
+        public string $classControl = '',
+        public string $validFeedback = '',
+        public string $invalidFeedback = '',
+        public bool $tooltipFeedback = false
+    ) {
+
+        $this->showErrors = $showErrors;
+
+        if ($language) {
+            $this->name = "{$name}[{$language}]";
+        }
+
+        $this->setValue($name, $bind, $default, $language);
+    }
 }
