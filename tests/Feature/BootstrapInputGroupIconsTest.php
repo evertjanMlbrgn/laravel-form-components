efa0816061572e8174f4260b69a54e8db30e45af
Evertjan Garretsen <evertjan@mlbrgn.nl>
2024-07-01 22:34:44 +0200

    cleanup and improved tests for input-group-icon component

--- tests/Feature/BootstrapInputGroupIconsTest.php
+++ tests/Feature/BootstrapInputGroupIconsTest.php
@@ -5,10 +5,10 @@ it('can render icons', function() {
 
     $this->visit('/bootstrap-input-group-icons')
         ->within('#form-input-group-icons', function() {
-            $this->seeElement('div.input-group label.input-group-text.input-group-icon i.bi-alarm')
-            ->seeElement('div.input-group label.input-group-text.input-group-icon img[alt=""]')
-            ->seeElement('div.input-group label.input-group-text.input-group-icon svg')
-            ->seeElement('div.input-group label.input-group-text.input-group-icon svg use');
+            $this->seeElement('div.input-group span.input-group-text.input-group-icon.input-group-icon-font i.bi-alarm')
+            ->seeElement('div.input-group span.input-group-text.input-group-icon.input-group-icon-img img[alt=""]')
+            ->seeElement('div.input-group span.input-group-text.input-group-icon.input-group-icon-svg svg')
+            ->seeElement('div.input-group span.input-group-text.input-group-icon.input-group-icon-sprite svg use');
         });
 });
 
@@ -17,7 +17,7 @@ it('sets extra classes', function() {
 
     $this->visit('/bootstrap-input-group-icons')
         ->within('#form-input-group-icons-extra-classes', function() {
-            $this->seeElement('div.mb-3 div.input-group.extra-1.extra-2 label.input-group-text.input-group-icon');
+            $this->seeElement('div.mb-3 div.input-group.extra-1.extra-2 span.input-group-text.input-group-icon.input-group-icon-font');
         });
 });
 
@@ -26,10 +26,10 @@ it('supports icon-position attribute', function() {
 
     $this->visit('/bootstrap-input-group-icons')
         ->within('#form-input-group-icons-icon-position', function() {
-            $this->seeElement('div div.input-group input.form-control + label.input-group-text.input-group-icon i.bi-alarm')
-            ->seeElement('div div.input-group input.form-control + label.input-group-text.input-group-icon img[alt=""]')
-            ->seeElement('div div.input-group input.form-control + label.input-group-text.input-group-icon svg')
-            ->seeElement('div div.input-group input.form-control + label.input-group-text.input-group-icon svg use');
+            $this->seeElement('div div.input-group input.form-control + span.input-group-text.input-group-icon.input-group-icon-font i.bi-alarm')
+            ->seeElement('div div.input-group input.form-control + span.input-group-text.input-group-icon.input-group-icon-img img[alt=""]')
+            ->seeElement('div div.input-group input.form-control + span.input-group-text.input-group-icon.input-group-icon-svg svg')
+            ->seeElement('div div.input-group input.form-control + span.input-group-text.input-group-icon.input-group-icon-sprite svg use');
         });
 });
 
@@ -38,6 +38,9 @@ it('supports for attribute', function() {
 
     $this->visit('/bootstrap-input-group-icons')
         ->within('#form-input-group-icons-for', function() {
-            $this->seeElement('div div.input-group input.form-control[id="test-input"] + label.input-group-text.input-group-icon[for="test-input"] i.bi-alarm');
+            $this->seeElement('div div.input-group input.form-control[id="input-1"] + label.input-group-text.input-group-icon.input-group-icon-font[for="input-1"] i.bi-alarm')
+            ->seeElement('div div.input-group input.form-control[id="input-2"] + label.input-group-text.input-group-icon.input-group-icon-img[for="input-2"] img[alt=""]')
+            ->seeElement('div div.input-group input.form-control[id="input-3"] + label.input-group-text.input-group-icon.input-group-icon-svg[for="input-3"] svg')
+            ->seeElement('div div.input-group input.form-control[id="input-4"] + label.input-group-text.input-group-icon.input-group-icon-sprite[for="input-4"] svg use');
         });
 });
