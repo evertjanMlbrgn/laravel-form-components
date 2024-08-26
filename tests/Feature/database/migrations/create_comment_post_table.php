3a2c6556ae495f0045ae2c452def82e26f3645f4
Evertjan Garretsen <evertjan@mlbrgn.nl>
2024-08-27 00:35:39 +0200

    small fixes

--- tests/Feature/database/migrations/create_comment_post_table.php
+++ tests/Feature/database/migrations/create_comment_post_table.php
@@ -11,7 +11,7 @@ class CreateCommentPostTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('comment_post', function (Blueprint $table) {
             $table->unsignedBigInteger('post_id');
@@ -24,7 +24,7 @@ class CreateCommentPostTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('comment_post');
     }
