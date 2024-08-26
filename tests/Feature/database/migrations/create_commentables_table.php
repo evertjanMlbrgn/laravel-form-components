4afd3c9952153581b24daec99c96383759f06d96
Evertjan Garretsen <evertjan@mlbrgn.nl>
2024-08-27 01:15:36 +0200

    added namespace to migrations in tests

--- tests/Feature/database/migrations/create_commentables_table.php
+++ tests/Feature/database/migrations/create_commentables_table.php
@@ -1,5 +1,7 @@
 <?php /** @noinspection SpellCheckingInspection */
 
+namespace Mlbrgn\LaravelFormComponents\Tests\Feature\Database\Migrations;
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
--- tests/Feature/database/migrations/create_comments_table.php
+++ tests/Feature/database/migrations/create_comments_table.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Mlbrgn\LaravelFormComponents\Tests\Feature\Database\Migrations;
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
