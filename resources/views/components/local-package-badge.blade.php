1461df522e841b24293be0878fb2e0f80562ac04
Evertjan Garretsen <evertjan@mlbrgn.nl>
2025-09-04 21:26:33 +0200

    also showing when using remote package

--- resources/views/components/local-package-badge.blade.php
+++ resources/views/components/local-package-badge.blade.php
@@ -1,3 +1,7 @@
-@if(config('form-components.mfc_using_local_package'))
-    <span class="mlbrgn-badge-local-package">USING LOCAL PACKAGE</span>
+@if(app()->environment('local'))
+    @if(config('form-components.mfc_using_local_package'))
+        <span class="mlbrgn-badge-local-package">USING LOCAL PACKAGE</span>
+    @else
+        <span class="mlbrgn-badge-remote-package">USING REMOTE PACKAGE</span>
+    @endif
 @endif
